1b70c3c6e8081721a4e1ba451b48b69106deb66f
Shavar <shavar.lmores@yahoo.com>
2019-04-17 23:32:51 -0400

    Update File.java

--- File.java
+++ File.java
@@ -6,11 +6,12 @@ public class File {
 
 
     public static void main(String[] args) throws IOException {
-        readFile("/Users/shavar/MaterialApps/LeetCode/src/main/java/FilesIO/sample.txt");
+     
         String data = "There are many variations of passages of Lorem Ipsum available \nbut the majority have suffered alteration in some form, by injected humour.";
-        // writeFile(data);
+         writeFile(data);
         System.out.println();
-        readFile("/Users/shavar/MaterialApps/LeetCode/src/main/java/FilesIO/lorem.txt");
+        //pass the path of the file you want to read 
+        readFile(path);
     }
 
     public static void readFile(String path) throws IOException {
@@ -19,9 +20,15 @@ public class File {
         System.out.println("Now reading from file: " + path);
 
         try {
+            //instantiated a new BufferedReader object
+            //which takes a fileReader object with a parameter of type string
+            //which will be used as the path of the file you want to read.
             bufferedReader = new BufferedReader(new FileReader(path));
+            //while th contents of the file is not null
+            //we will read eachline until its empty.
             while ((lineToRead = bufferedReader.readLine()) != null) {
                 System.out.printf("%s\n", lineToRead);
+                //here we are just printing the contents of the file out to the console.
             }
 
         } catch (FileNotFoundException ex) {
@@ -41,22 +48,31 @@ public class File {
         FileWriter fileWriter = null;
         java.io.File file = new java.io.File("Demo.txt");
         try {
+               //first we want to check if the file already exist, 
+                //if not we create a new file.
             if (!file.exists()) {
                 file.createNewFile();
                 System.out.println();
+                //print a message to the console where the filePath is located and also
+                //append a message stating that the file is created.
                 System.out.println(file.getAbsoluteFile() + " is now created");
 
             }
+            //pass the path of the file we want to write to the filewriter object
             fileWriter = new FileWriter(file.getAbsoluteFile());
             bufferedWriter = new BufferedWriter(fileWriter);
+            //the bufferedReader then starts to write the content to the file.
             bufferedWriter.write(data);
             System.out.println();
+            //lastly we just print a message to the console when the file is finished being written
+            //along with the path of the file where its located.
             System.out.print("Finished Writing to file " + file.getAbsolutePath() + ".");
 
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
             bufferedWriter.close();
+            
         }
 
     }
